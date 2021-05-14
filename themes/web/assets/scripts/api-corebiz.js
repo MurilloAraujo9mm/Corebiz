7905ed88d3c41c725779c86bd647afaa0a7497dc
Murillo Araujo .G <murilloaraujog@gmail.com>
2021-05-14 18:17:32 -0300

    carousel development

--- themes/web/assets/scripts/api-corebiz.js
+++ themes/web/assets/scripts/api-corebiz.js
@@ -22,21 +22,54 @@ $(document).ready(function () {
                 let promotion = quantity ? `<p>ou em ${quantity}x de R$ ${value} </p>` : '';
                 let off = product.productId == 2 || product.productId == 3 ? `<div class="polygon"><p>off</div>` : '';
 
-                $(".section-products").append(`
-                    <article class="section-products-item">
-                        <img class="section-products-image" src="${product.imageUrl}">
+                $(".carrosel").prepend(`
+                    <div class="section-products-item">
+                        <a class="item" href="#"><img style="margin-right: 0px" class="section-products-image" src="${product.imageUrl}"></a>
                         <header>
-                            <h1>${product.productName}</h1>
+                        <h1>${product.productName}</h1>
                         </header>
                         <p>${set_start_item(product)}</p>
                         <p>Por R$ ${product.price.toFixed(2)}</p> ${promotion}
                         <button class="btn-buy" buy-product="true">Comprar</button>
                         <span>${off}</span>
-                    </article>
-               `);
+                    </div>
+                `);
             });
+            carrouselOfProducts();
         }
     });
+
+    const carrouselOfProducts = () => {
+        let width = (parseInt($('.carrosel .item').outerWidth()) + parseInt($(".carrosel .item").css("margin-right"))) * $(".carrosel .item").length;
+        $(".carrosel").css("width", width);
+
+        let number_images = 3;
+        let padding_and_margin = 80;
+        let next_or_prev = 0;
+        let count = ($(".carrosel .item").length / number_images) - 1;
+
+        let slide = (number_images * padding_and_margin) + ($('.carrosel img').outerWidth()) * number_images;
+
+        $(".next").click(function () {
+
+            if (next_or_prev < count) {
+                next_or_prev++;
+                $(".carrosel").animate({
+                    marginLeft: '-=' + slide + 'px'
+                }, '500')
+            }
+        });
+
+        $(".prev").click(function () {
+
+            if (next_or_prev >= 1) {
+                next_or_prev--;
+                $(".carrosel").animate({
+                    marginLeft: '+=' + slide + 'px'
+                }, '500')
+            }
+        });
+    }
 
     $("body").on("click", "[buy-product]", function (event) {
         buy++;
--- themes/web/assets/scripts/script.js
+++ themes/web/assets/scripts/script.js
@@ -198,4 +198,9 @@ $(function () {
         }
     });
 
+
+    const slideProduct = () => {
+        
+    }
+
 });
